d01b3337d8317fb300bcc3c22e04ce8575fd03e5
animegirls <daobinhminh091097@gmail.com>
2019-12-02 12:23:36 +0700

    practical work 11

--- src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -8,10 +8,18 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.util.Log;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class WeatherActivity extends AppCompatActivity {
@@ -52,12 +60,40 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i("InfoTag", "onCreate");;
 
         PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        ViewPager pager = findViewById(R.id.pager);
+        TabLayout tabLayout = findViewById(R.id.tab);
+
+        copyFileToExternalStorage(R.raw.miiplaza, "miiplaza.mp3");
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.miiplaza);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
 
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
         tabLayout.setupWithViewPager(pager);
+    }
+
+    private void copyFileToExternalStorage(int resourceId, String resourceName){
+        String pathSDCard = Environment.getExternalStorageDirectory() + "/Android/data/vn.edu.usth.usthweather/" + resourceName;
+        try{
+            InputStream in = getResources().openRawResource(resourceId);
+            FileOutputStream out = null;
+            out = new FileOutputStream(pathSDCard);
+            byte[] buff = new byte[1024];
+            int read = 0;
+            try {
+                while ((read = in.read(buff)) > 0) {
+                    out.write(buff, 0, read);
+                }
+            } finally {
+                in.close();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
