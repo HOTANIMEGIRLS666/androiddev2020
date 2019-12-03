95d69db958cf0db952c020911257b24245fbc5cd
animegirls <daobinhminh091097@gmail.com>
2019-12-03 21:22:24 +0700

    practical work 12

--- src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Environment;
 import android.util.Log;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -16,7 +20,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 
 public class WeatherActivity extends AppCompatActivity {
@@ -29,15 +32,14 @@ public class WeatherActivity extends AppCompatActivity {
         PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
         ViewPager pager = findViewById(R.id.pager);
         TabLayout tabLayout = findViewById(R.id.tab);
-
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.miiplaza);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         copyFileToExternalStorage(R.raw.miiplaza, "miiplaza.mp3");
-        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.miiplaza);
         mediaPlayer.start();
         mediaPlayer.setLooping(true);
 
+        setSupportActionBar(toolbar);
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
         tabLayout.setupWithViewPager(pager);
@@ -64,6 +66,28 @@ public class WeatherActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tool_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                Toast.makeText(this, "REFRESHING!!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_more:
+                Intent intent0 = new Intent(this,PrefActivity.class);
+                startActivity(intent0);
+                return true;
+            default:
+                super.onOptionsItemSelected(item);
+        }
+        return false;
     }
 
     @Override
